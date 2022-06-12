8a9d82266c88452609b5f6e09698ab3e3398d37a
danya1648 <93270920+danya1648@users.noreply.github.com>
2022-06-12 22:32:30 +0300

    Update Age.py

--- Age.py
+++ Age.py
@@ -1,7 +1,10 @@
-age = 10
-if age < 10:
-	print("Kid")
-elif (age <= 10 or age != 18):
-	print("Teenager")
-else:
-	print("Adult")
+def age_func():
+	
+	age = 10
+	
+	if age < 10:
+		print("Kid")
+	elif (age <= 10 or age != 18):
+		print("Teenager")
+	else:
+		print("Adult")
